e533d888678f6754331913407fd88c33343f98cd
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-04-16 21:51:13 -0500

    Delete final_project_data directory

--- final_project_data/scripts/cleanup.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE albums;        
-DROP TABLE genres;           
-DROP TABLE r_artist_genre;
-DROP TABLE artists;
-DROP TABLE r_albums_artists;  
-DROP TABLE r_track_artist;
-DROP TABLE audio_features;    
-DROP TABLE r_albums_tracks;
-DROP TABLE tracks;
