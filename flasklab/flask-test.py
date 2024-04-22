f718c91a4f0b11430c7973ff7dbe0346070e13d0
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-04-22 10:27:16 -0500

    transfer to stearns

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -16,15 +16,23 @@ def my_display(word1, word2):
 def my_color(word1):
     return '<h1 style="color:Red">' + word1 + '</h1>'
 
+@app.route('/pop/<place>')
+def get_population(place):
+    query = f"SELECT state_pop FROM states WHERE state_name = '{place}'"
+    
+
 @app.route("/add/<x>/<y>")
 def add(x, y):
-    x, y, = float(x), float(y) # cast to floats first in case they're decimal values
+    # Convert to numbers:
+    x = float(x) if '.' in x else int(x)
+    y = float(y) if '.' in y else int(y)
     z = x + y
-    # is it an int?
+    # is our result an int?
     if z.is_integer():
-        return int(z)
-    return z
+        z = int(z)
+    # Return with fun colors:
+    return f"<h1> <span style='color:Red'>{x}</span> + <span style='color:Green'>{y}</span> = <span style='color:Blue'>{z}</span> </h1>"
 
 if __name__ == '__main__':
-    my_port = 4090
+    my_port = 5115 # or 5215
     app.run(host='0.0.0.0', port = my_port) 
